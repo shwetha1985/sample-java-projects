1d2184727b846d92b2c3db28887336f59707d4fd
shwetha <shwetha.krishnashetty@harman.com>
2019-06-13 12:03:04 +0530

    commiting sample.java

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 public class sample {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Jenkin Java code");
+      System.out.println("Jenkin Java code update1");
    }
 }
