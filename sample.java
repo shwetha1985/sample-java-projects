a959c166e71241923df2ad16e3088af060f93be1
shwetha <shwetha.krishnashetty@harman.com>
2019-06-14 12:29:26 +0530

    commit dev

--- sample.java
+++ sample.java
@@ -2,6 +2,6 @@ public class sample {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Jenkin Java code update1");
-	  System.out.println("Jenkin Java code Modified");
+	  System.out.println("Jenkin Java code Modified in development");
    }
 }
